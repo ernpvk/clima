eac844b76d220be90a51e93a467c1c940573df1b
earaen <123929693+earaen@users.noreply.github.com.>
2024-12-07 20:17:05 +0700

    feat: Implement weather data fetching

---
+++ src/js/service.js
@@ -0,0 +1,20 @@
+export async function fetchWeatherData(location) {
+  const API_KEY = process.env.API_KEY;
+  const BASE_URL =
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline";
+  const requestUrl = `${BASE_URL}/${location}?unitGroup=us&key=${API_KEY}&contentType=json`;
+
+  try {
+    console.log(`Fetching weather data for: ${location}`);
+    const response = await fetch(requestUrl, { mode: "cors" });
+
+    if (!response.ok) {
+      throw new Error(`Error ${response.status}: ${response.statusText}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Failed to fetch weather data:", error.message);
+    throw error;
+  }
+}
