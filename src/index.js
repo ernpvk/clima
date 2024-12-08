45f1902f0b08704445d5ec1df8eb523127e9cca7
earaen <123929693+earaen@users.noreply.github.com.>
2024-12-09 04:25:55 +0700

    feat: Add today's forecast slider and tempdata

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import { setupSearch } from "./js/setupSearch";
 import { renderAside } from "./js/asideRenderer";
+import { renderForecast } from "./js/forecastRenderer";
 import "./style.css";
 
 setupSearch((data, error) => {
   if (data) {
     renderAside(data);
+    renderForecast(data);
   } else if (error) {
     document.querySelector(".city-name").textContent = error;
   }
---
+++ src/js/forecastRenderer.js
@@ -0,0 +1,24 @@
+export function renderForecast(data) {
+  const forecastList = document.getElementById("forecast-list");
+  data.days[0].hours.forEach((hour) => {
+    const forecastCard = document.createElement("div");
+    forecastCard.classList.add("forecast-card");
+    forecastCard.textContent = hour.temp;
+    forecastList.appendChild(forecastCard);
+  });
+}
+
+// Slider
+const forecastList = document.getElementById("forecast-list");
+const leftButton = document.querySelector(".left-btn");
+const rightButton = document.querySelector(".right-btn");
+
+const scrollAmount = 200;
+
+leftButton.addEventListener("click", () => {
+  forecastList.scrollLeft -= scrollAmount;
+});
+
+rightButton.addEventListener("click", () => {
+  forecastList.scrollLeft += scrollAmount;
+});
