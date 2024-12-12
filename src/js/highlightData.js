11f89f36b5c9a7b41b5ec032b6b151e110c418a0
earaen <123929693+earaen@users.noreply.github.com.>
2024-12-13 01:40:40 +0700

    feat: add highlight data

--- src/js/highlightData.js
+++ src/js/highlightData.js
@@ -1,10 +1,32 @@
+import { getUVInterpretation } from "./utils";
 export function getHighlight(data) {
   const highlightList = document.querySelector(".highlight-list");
   const feeltemp = document.getElementById("feel-temp");
   const tempMax = document.getElementById("temp-max");
   const tempMin = document.getElementById("temp-min");
 
-  tempMax.textContent = `Max Temp${data.days[0].tempmax}`;
-  tempMin.textContent = data.days[0].tempmin;
-  feeltemp.textContent = data.days[0].feelslike;
+  tempMax.textContent = `High ${data.days[0].tempmax}`;
+  tempMin.textContent = `Low ${data.days[0].tempmin}`;
+  feeltemp.textContent = `Feels like 
+   ${data.days[0].feelslike}`;
+
+  if (data.days[0].preciptype == "rain") {
+    const rainPercentage = document.getElementById("rain-percentage");
+    rainPercentage.textContent = `${data.days[0].precipprob}`;
+  }
+
+  const conditions = document.getElementById("conditions");
+  conditions.textContent = data.days[0].conditions;
+  const cover = document.getElementById("cover");
+  cover.textContent = `${data.days[0].cloudcover}%`;
+
+  const windSpeed = document.getElementById("wind-speed");
+  windSpeed.textContent = `${data.days[0].windspeed} mph`;
+
+  const uv = document.getElementById("uv");
+  const uvIndex = data.days[0].uvindex;
+  uv.textContent = uvIndex;
+
+  const uvInterpret = document.getElementById("uvInterpret");
+  uvInterpret.textContent = getUVInterpretation(uvIndex);
 }
--- src/js/utils.js
+++ src/js/utils.js
@@ -15,3 +15,27 @@ export function getDayName(dateString) {
   const date = new Date(dateString);
   return date.toLocaleDateString("en-US", { weekday: "short" });
 }
+
+export function getUVInterpretation(index) {
+  if (index >= 0 && index < 3) {
+    uv.style.backgroundColor = "green";
+    uv.style.color = "white";
+    return "Low (Minimal protection needed)";
+  } else if (index >= 3 && index < 6) {
+    uv.style.backgroundColor = "yellow";
+    uv.style.color = "black";
+    return "Moderate (Some protection recommended)";
+  } else if (index >= 6 && index < 8) {
+    uv.style.backgroundColor = "orange";
+    uv.style.color = "white";
+    return "High (Protection essential)";
+  } else if (index >= 8 && index < 11) {
+    uv.style.backgroundColor = "red";
+    uv.style.color = "white";
+    return "Very High (Extra protection needed)";
+  } else if (index >= 11) {
+    uv.style.backgroundColor = "violet";
+    uv.style.color = "white";
+    return "Extreme (Avoid being outside)";
+  }
+}
