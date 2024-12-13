9785cd9be1e21a94ab07bd9cc24bed8b062e597c
earaen <123929693+earaen@users.noreply.github.com.>
2024-12-13 18:21:40 +0700

    fix: get back the older folder for dist

--- webpack.common.js
+++ webpack.common.js
@@ -1,3 +1,8 @@
+const path = require("path");
+const webpack = require("webpack");
+require("dotenv").config();
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   entry: "./src/index.js",
   output: {
@@ -29,16 +34,10 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
-        generator: {
-          filename: "assets/images/[name][ext]",
-        },
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
-        generator: {
-          filename: "assets/fonts/[name][ext]",
-        },
       },
     ],
   },
