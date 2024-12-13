54c09419026b77266cf0b4f17ef61133999d207e
earaen <123929693+earaen@users.noreply.github.com.>
2024-12-13 18:15:42 +0700

    style(assets): add cloud and page loadet

--- src/js/setupSearch.js
+++ src/js/setupSearch.js
@@ -3,17 +3,29 @@ import { fetchWeatherData } from "./weatherService";
 export async function setupSearch(callback) {
   const cityInput = document.querySelector(".search-input");
   const searchButton = document.querySelector(".aside-header button");
+  const loaderContainer = document.querySelector(".loader-container");
+
+  const showLoader = () => {
+    loaderContainer.classList.remove("hidden");
+  };
+
+  const hideLoader = () => {
+    loaderContainer.classList.add("hidden");
+  };
 
   const handleSearch = async (cityName) => {
     const city = cityName || cityInput.value.trim();
     if (city) {
       try {
+        showLoader();
         const data = await fetchWeatherData(city);
         cityInput.value = "";
         callback(data);
       } catch (error) {
         console.error("Error fetching data:", error);
         alert("City not found");
+      } finally {
+        hideLoader();
       }
     } else {
       alert("Please enter a city name.");
