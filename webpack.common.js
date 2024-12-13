eda9bb1ab991d4958b90f9619136ca2c83a1f2b4
earaen <123929693+earaen@users.noreply.github.com.>
2024-12-13 18:18:57 +0700

    fix: change folder build for assets

--- webpack.common.js
+++ webpack.common.js
@@ -1,8 +1,3 @@
-const path = require("path");
-const webpack = require("webpack");
-require("dotenv").config();
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-
 module.exports = {
   entry: "./src/index.js",
   output: {
@@ -34,10 +29,16 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
+        generator: {
+          filename: "assets/images/[name][ext]",
+        },
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
+        generator: {
+          filename: "assets/fonts/[name][ext]",
+        },
       },
     ],
   },
